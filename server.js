a715c8b4bc5009861813a75ef713eb81968f5cc2
Kenneth Kopelson <kennethkopelson@gmail.com>
2021-02-05 16:00:52 -0800

    initialize mongodb and mongoose setup, create Books model

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,6 +11,12 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // Define API routes here
 
