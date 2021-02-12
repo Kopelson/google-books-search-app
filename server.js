75ecf8019ac80796783a51f3701eacb7d9d37491
Kenneth Kopelson <kennethkopelson@gmail.com>
2021-02-12 15:36:26 -0800

    added server code

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(bodyParser.json());
+app.use(cors());
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
