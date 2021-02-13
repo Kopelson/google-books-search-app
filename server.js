0e983c05143fd5a263baaa7ed88a2bd23f5d7e38
Kenneth Kopelson <kennethkopelson@gmail.com>
2021-02-12 16:32:08 -0800

    changed db name

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBooksDB", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
